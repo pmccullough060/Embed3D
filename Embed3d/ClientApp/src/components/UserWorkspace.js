e2981dd5bf54144d270dc577716319358cd92e2c
pmccullough060 <pmccullough060@gmail.com>
2021-05-05 19:57:53 +0100

    updated models and added UserWorkspace React Component

---
+++ Embed3d/ClientApp/src/components/UserWorkspace.js
@@ -0,0 +1,14 @@
+﻿import React, { Component } from 'react';
+import authService from './api-authorization/AuthorizeService'
+import { FetchData } from './FetchData';
+
+export class UserWorkspace extends Component {
+
+    //Gets the users current name.
+    static displayName = FetchData.name;
+
+
+
+
+
+}
